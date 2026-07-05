8660bf644d1eff58b79adb8810d7ea2152c7628f
Daniil Kamaev <V1lliard@yandex.ru>
2026-07-05 12:32:37 +0300

    обновил config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/nextJS-photostudio',
+  assetPrefix: '/nextJS-photostudio'
 };
 
 export default nextConfig;
